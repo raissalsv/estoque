b40d675ff43b2bf8be83acc82b1744e56dd715ea
Raissa Lima <raissa.lima@actualsales.eu>
2018-08-22 12:59:23 -0300

    CSS - bootstrap and fontawesome icons

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -2,6 +2,7 @@
 <head>
   <link href="/css/app.css" rel="stylesheet">
   <link href="/css/custom.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
   <title>Controle de estoque</title>
 </head>
--- resources/views/produto/listagem.blade.php
+++ resources/views/produto/listagem.blade.php
@@ -8,12 +8,12 @@
 <h1>Listagem de Produtos</h1>
 <table class="table table-striped table-bordered table-hover">
   @foreach ($produtos as $p)
-    <tr class="{{$p->quantidade == 1 ? 'danger' : '' }}">
+    <tr class="{{$p->quantidade == 1 ? 'bg-danger' : '' }}">
       <td>{{$p->nome}}</td>
       <td>{{$p->valor}}</td>
       <td>{{$p->quantidade}}</td>
       <td>{{$p->descricao}}</td>
-      <td><a href="/produtos/mostra/{{$p->id}}">Ver</a></td>
+        <td><a href="/produtos/mostra/{{$p->id}}"><i class="fab fa-searchengin"></i></a></td>
     </tr>
   @endforeach
 </table>
