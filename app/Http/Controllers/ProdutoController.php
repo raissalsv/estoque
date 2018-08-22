51ccb18ab94f790c17b55495438ddb1b53e98975
Raissa Lima <raissa.lima@actualsales.eu>
2018-08-22 19:09:34 -0300

    Chapter 8

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -8,20 +8,39 @@ use estoque\Produto;
 
 class ProdutoController extends Controller
 {
-  public function lista(){
-    $produtos = DB::select('select * from produtos');
-    return view('produto/listagem')->with('produtos', $produtos);
-  }
-
-  public function mostra($id){
-
-    $resposta = DB::select('select * from produtos where id = ?', [$id]);
-    if(empty($resposta)) {
-      return "Esse produto não existe";
+    public function lista()
+    {
+        $produtos = DB::select('select * from produtos');
+        return view('produto/listagem')->with('produtos', $produtos);
     }
-    return view('produto/detalhes')->with('p', $resposta[0]);
 
-  }
+    public function mostra($id)
+    {
+
+        $resposta = DB::select('select * from produtos where id = ?', [$id]);
+        if (empty($resposta)) {
+            return "Esse produto não existe";
+        }
+        return view('produto/detalhes')->with('p', $resposta[0]);
+
+    }
+
+    public function novo()
+    {
+        return view('produto/formulario');
+    }
+
+    public function adiciona()
+    {
+        $nome = Request::input('nome');
+
+        $valor = Request::input('valor');
+        $descricao = Request::input('descricao');
+        $quantidade = Request::input('quantidade');
+        DB::insert('insert into produtos (nome, quantidade, valor, descricao) values (?,?,?,?)',
+            array($nome, $quantidade, $valor, $descricao ));
+        return view('produto.adicionado')->with('nome', $nome);
+    }
 
 
 }
---
+++ resources/views/produto/formulario.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.principal')
+
+@section('conteudo')
+
+    <h1>Novo Produto</h1>
+
+    <form action="/produtos/adiciona" method="POST">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+        <div class="form-group">
+            <label>Nome</label>
+            <input name="nome" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label>Descricao</label>
+
+            <input name="descricao" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label>Valor</label>
+            <input name="valor" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label>Qauntidade</label>
+            <input type="number" name="quantidade" class="form-control"/>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+
+
+@stop
